09a98ba149eb3c8b8ed49a9accf5b4e5ae387f02
paskuale <pellicanipasquale@gmail.com>
2018-07-13 09:51:01 +0200

    sb-admin site/index view added

---
+++ views/sb-admin/site/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * index.php
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2017
+ * @link https://github.com/p2made
+ * @package yii2-startbootstrap-themes
+ * @license MIT
+ */
+
+/* @var $this yii\web\View */
+
+use yii\bootstrap\Html;
+use p2m\helpers\FA;
+
+$this->title = 'Home';
+$assetDir = Yii::$app->assetManager->getPublishedUrl(
+	'@vendor/p2made/yii2-startbootstrap-themes/assets/lib/sb-admin'
+);
+?>
+
+	<!-- Put your page content here! -->
+
