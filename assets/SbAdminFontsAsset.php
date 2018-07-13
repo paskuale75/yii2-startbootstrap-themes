8693e7f0424a375cfad6666292c21deeb50bd72e
paskuale <pellicanipasquale@gmail.com>
2018-07-13 09:45:38 +0200

    sb-admin added to theme's list

---
+++ assets/SbAdminFontsAsset.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * SbAdminFontsAsset.php
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2017
+ * @link https://github.com/p2made
+ * @package p2made/yii2-startbootstrap-themes
+ * @license MIT
+ */
+
+/**
+ * Load this asset with...
+ * p2m\SB\assets\SbAdminFontsAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\SB\assets\_ExampleFontsAsset',
+ */
+
+namespace p2m\SB\assets;
+
+class SbAdminFontsAsset extends \p2m\assets\base\P2AssetBundle
+{
+    protected $resourceData = array(
+
+        'static' => [
+            'baseUrl' => '//fonts.googleapis.com',
+            'css' => [
+                '__font__',
+            ],
+            'js' => [],
+        ],
+
+        'depends' => [
+            'p2m\assets\P2CoreAsset',
+        ],
+    );
+
+    public function init()
+    {
+        $this->configureAsset($this->resourceData);
+        parent::init();
+    }
+}
